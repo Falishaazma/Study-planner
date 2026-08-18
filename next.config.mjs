163b85281aaca16bff8507cfdcc3fc23fb4ed1b0
Falishaazma <86997938+Falishaazma@users.noreply.github.com>
2026-08-18 18:34:33 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/Study-planner',
   images: {
     unoptimized: true,
   },
 };
 
 export default nextConfig;
+
